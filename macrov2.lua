ff7f592f94db3a2e35caf22a8d55b692661e5432
Narnia1337 <102632186+Narnia1337@users.noreply.github.com>
2022-11-24 01:01:33 -0600

    Create macrov2.lua

---
+++ macrov2.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 1537690962 then
+  loadstring(game:HttpGet('https://scripts.luawl.com/12761/bss.lua'))()
+end
+
+if game.PlaceId == 1962086868 then
+  loadstring(game:HttpGet('https://scripts.luawl.com/13623/TowerofHell.lua'))()
+end
