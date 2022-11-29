3fb010d06f76c389fda89e67b5d9189427a37a79
Narnia1337 <102632186+Narnia1337@users.noreply.github.com>
2022-11-29 01:15:31 -0600

    Update macrov2.lua

--- macrov2.lua
+++ macrov2.lua
@@ -1,7 +1,3 @@
 if game.PlaceId == 1537690962 then
   loadstring(game:HttpGet('https://scripts.luawl.com/12761/bss.lua'))()
 end
-
-if game.PlaceId == 1962086868 then
-  loadstring(game:HttpGet('https://scripts.luawl.com/13623/TowerofHell.lua'))()
-end
