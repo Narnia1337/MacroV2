78789d42f350d2bfd29145c2232aadcb6a08b22a
Narnia1337 <102632186+Narnia1337@users.noreply.github.com>
2023-02-17 01:27:32 -0600

    Update macrov2-buyer.lua

--- macrov2-buyer.lua
+++ macrov2-buyer.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ef9c9d53ed41ef4f9b2a80683d72679b.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/171c51361badcb7dc31650620c2bd82d.lua"))()
