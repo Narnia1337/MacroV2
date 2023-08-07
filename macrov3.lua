f3a69254681445b9c53b2702e4bd69ecbcd7544b
Narnia1337 <102632186+Narnia1337@users.noreply.github.com>
2023-08-06 19:18:22 -0500

    Update macrov3.lua

--- macrov3.lua
+++ macrov3.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e2bcc9dc4b2dfb8549658edff42ef855.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/bc8c5c45c26f8e4cdb060277b0bbcab7.lua"))()
+
